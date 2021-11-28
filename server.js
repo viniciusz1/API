a441736e55fab5b3116f78eb3f600a46c78f7799
viniciusz1 <vinicius_benner@estudante.sesisenai.org.br>
2021-11-28 12:24:58 -0300

    g

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // sendFile will go here
-app.get('/', function(requisicao, resposta) {
-  resposta.sendFile(path.join(__dirname, '/index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 app.use(express.static(__dirname + '/public'));
 app.listen(port);

2739c9ad190f8b4b9707dca6c2947fc6b95a7b63
viniciusz1 <vinicius_benner@estudante.sesisenai.org.br>
2021-11-28 11:54:30 -0300

    f

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // sendFile will go here
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.get('/', function(requisicao, resposta) {
+  resposta.sendFile(path.join(__dirname, '/index.html'));
 });
-
+app.use(express.static(__dirname + '/public'));
 app.listen(port);
 console.log('Server started at http://localhost:' + port);

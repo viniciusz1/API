c8547290f711d21593654f8782c8c0da44e242a1
Vinícius Bonatti Benner <92129993+viniciusz1@users.noreply.github.com>
2021-11-30 19:50:02 -0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -59,7 +59,7 @@ function showPersonTable() {
             
           if (td || td2) {
             txtValue = td.textContent || td.innerText;
-            txtValue2 = td.textContent || td.innerText;
+            txtValue2 = td2.textContent || td2.innerText;
             if ((txtValue.toUpperCase().indexOf(filter)|| txtValue2.toUpperCase().indexOf(filter)) > -1) {
               tr[i].style.display = "";
             } else {
